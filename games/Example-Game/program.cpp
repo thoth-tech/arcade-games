7c2b8746360c1eddff379d19a905e5a14f7dfbb9
Delcari <delcari@protonmail.com>
2022-09-05 16:40:19 +1000

    new(game): boilerplate 'game' to demo script

---
+++ games/Example-Game/program.cpp
@@ -0,0 +1,14 @@
+#include "splashkit.h"
+
+int main()
+{
+	open_window("Example Game",600,400);
+	while(!quit_requested() && !key_typed(ESCAPE_KEY))
+	{
+		clear_screen(COLOR_BLUE);
+		refresh_screen(60);
+		process_events();
+	}
+
+    return 0;
+}
