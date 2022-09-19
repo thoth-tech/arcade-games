29debaf06f3e61fbb50aa19cf3678d3875417efd
Riley Dellios <delcari@protonmail.com>
2022-09-19 21:00:03 +1000

    test(compile-games): modify example game to test modifications

--- games/Example-Game/program.cpp
+++ games/Example-Game/program.cpp
@@ -5,7 +5,7 @@ int main()
 	open_window("Example Game",600,400);
 	while(!quit_requested() && !key_typed(ESCAPE_KEY))
 	{
-		clear_screen(COLOR_GREEN);
+		clear_screen(COLOR_YELLOW);
 		refresh_screen(60);
 		process_events();
 	}
