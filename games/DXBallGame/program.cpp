48f95cc26b2c07163123105132539f3526e57e8a
U-SWANOS\Swanos <swanny.aurellia21@gmail.com>
2023-03-28 22:15:36 +1100

    Swanny - DX Ball Game

--- games/DXBallGame/program.cpp
+++ games/DXBallGame/program.cpp
@@ -2,6 +2,14 @@
 
 int main()
 {
-    write_line("Hello World!");
+    open_window("DX Ball Game", 800, 600);
+    window_toggle_border("DX Ball Game");
+
+    while(!key_down(ESCAPE_KEY))
+    {
+        // game play
+        clear_screen(COLOR_BLACK);
+        refresh_screen(60);
+    }
     return 0;
 }
