336271abd6f89cec123d56c0169af1ea67b0ad65
Norman Harty <87473593+Norman-Harty@users.noreply.github.com>
2023-03-29 23:52:58 +1100

    Update program.cpp
    added player movement, block breaking, score increasing, level completing and level failure.

--- games/DXBallGame/program.cpp
+++ games/DXBallGame/program.cpp
@@ -1,10 +1,19 @@
 #include "splashkit.h"
+using namespace std;
+/*
+ * Todo:
+ *
+ * fix diagonal collisions (if the ball collides with a block diagonally it seems to pass through the block until it hits the center)
+ * add more levels and ability to start again after losing
+ * maybe add a game data struct to store game state info
+ */
 
 const int BLOCKS_IN_LEVEL1 = 110; // Number of blocks to be spawned in level 1
 const double BLOCK_WIDTH = 60; // Block width
 const double BLOCK_HEIGHT = 20; // Block height
 const double BALL_RADIUS = 10; // Radius of ball
 const double BALL_SPEED = 2; // Speed of ball movement
+const double PADDLE_SPEED = 5; //Speed of paddle movement
 const double PADDLE_Y = 550; // Location of paddle on the y axis
 const double PADDLE_HEIGHT = 5; // Height of the paddle
 const double PADDLE_LENGTH = 100; // Length of the paddle
@@ -13,6 +22,7 @@ struct block_data
 {
     double x; // Location on the x axis
     double y; // Location on the y axis
+	bool broken = false; //whether or not the ball has hit the brick
 };
 
 struct ball_data
@@ -23,25 +33,54 @@ struct ball_data
     bool right; // Direction horizontally, either right or left
 };
 
+void end_level(bool successful)
+{
+	if (successful)
+	{
+		draw_text("You win!", COLOR_WHITE, font_named("default"), 50, 250, 300);
+		//progress to next level
+	}
+	else
+	{
+		draw_text("You lose, try again?", COLOR_WHITE, font_named("default"), 50, 100, 300);
+		//restart level
+	}
+}
+
 void draw_blocks(block_data blocks[], int remaining_blocks)
 {
     // Draw blocks to the screen
-    for (int i = 0; i < remaining_blocks; i++)
+    for (int i = 0; i < BLOCKS_IN_LEVEL1; i++)
     {
-        fill_rectangle(COLOR_BLUE, blocks[i].x, blocks[i].y, BLOCK_WIDTH, BLOCK_HEIGHT);
-        draw_rectangle(COLOR_BLACK, blocks[i].x, blocks[i].y, BLOCK_WIDTH, BLOCK_HEIGHT);
+		if (!blocks[i].broken) //broken blocks aren't drawn
+		{
+			fill_rectangle(COLOR_BLUE, blocks[i].x, blocks[i].y, BLOCK_WIDTH, BLOCK_HEIGHT);
+			draw_rectangle(COLOR_BLACK, blocks[i].x, blocks[i].y, BLOCK_WIDTH, BLOCK_HEIGHT);
+		}
     }
+}
+
+void break_block(block_data blocks[], int block_index, int &score, int &remaining_blocks)
+{
+	blocks[block_index].broken = true;
+	score++;
+	remaining_blocks--;
 }
 
 int main()
 {
+	load_resource_bundle("game_bundle", "bundle.txt");
     open_window("DX Ball Game", 800, 600);
     window_toggle_border("DX Ball Game");
-
+	
     // Spawn blocks
     int remaining_blocks = BLOCKS_IN_LEVEL1; // Number of blocks remaining
+	int score = 0;
+	bool game_over = false;
+	bool game_won = false;
     block_data blocks[BLOCKS_IN_LEVEL1]; // Array containing blocks to be destroyed
     int index = 0;
+
     for (int i = 0; i < 5; i++) // draw 10 layers
     {
         for (int j = 0; j < 10; j++) // draw 10 blocks
@@ -61,6 +100,15 @@ int main()
             index++;
         }
     }
+	
+	// spawn only one block for testing winning a level
+	/*
+	block_data block; 
+    block.x = 600;
+    block.y = 300;
+    blocks[index] = block;
+	*/
+	
     // Spawn ball at starting location
     ball_data ball;
     ball.x = screen_width()/2;
@@ -79,28 +127,49 @@ int main()
 
     while(!key_down(ESCAPE_KEY))
     {
+		process_events(); //check keyboard state
+		
         // Player controls
-
+		if (key_down(LEFT_KEY) and paddle_x > 10) paddle_x -= PADDLE_SPEED; //moving left
+		if (key_down(RIGHT_KEY) and paddle_x < screen_width() - PADDLE_LENGTH - 10) paddle_x += PADDLE_SPEED; //moving right
+		
         // Change ball movement
         // Bounce off walls
         if (ball.x - BALL_RADIUS <= 0) ball.right = true; // Bounce off left wall
         if (ball.x + BALL_RADIUS >= screen_width()) ball.right = false; // Bounce off right wall
         if (ball.y - BALL_RADIUS <= 0) ball.up = false; // Bounce off upper wall
-        if (ball.y + BALL_RADIUS >= screen_height()) ball.up = true; // Bounce off bottom wall (to be removed)
+        if (ball.y + BALL_RADIUS >= screen_height() and !game_over) // Hit bottom of the screen
+		{
+			//lose the game
+			game_won = false;
+			game_over = true;
+		}
         // Bounce off paddle
         if (ball.y + BALL_RADIUS == PADDLE_Y and (ball.x + BALL_RADIUS >= paddle_x and ball.x - BALL_RADIUS <= paddle_x + PADDLE_LENGTH))
             ball.up = true;
         // Bounce off blocks
-        for (int i = 0; i < remaining_blocks; i++)
+        for (int i = 0; i < BLOCKS_IN_LEVEL1; i++)
         {
-            if (ball.y - BALL_RADIUS == blocks[i].y + BLOCK_HEIGHT and (ball.x >= blocks[i].x and ball.x <= blocks[i].x + BLOCK_WIDTH)) 
-                ball.up = false; // Bounce off bottom of block
-            if (ball.y + BALL_RADIUS == blocks[i].y and (ball.x >= blocks[i].x and ball.x <= blocks[i].x + BLOCK_WIDTH)) 
-                ball.up = true; // Bounce off top of block
-            if (ball.x - BALL_RADIUS == blocks[i].x + BLOCK_WIDTH and (ball.y >= blocks[i].y and ball.y <= blocks[i].y + BLOCK_HEIGHT)) 
-                ball.right = true; // Bounce off right of block
-            if (ball.x + BALL_RADIUS == blocks[i].x and (ball.y >= blocks[i].y and ball.y <= blocks[i].y + BLOCK_HEIGHT)) 
-                ball.right = false; // Bounce off left of block
+            if (ball.y - BALL_RADIUS == blocks[i].y + BLOCK_HEIGHT and (ball.x >= blocks[i].x and ball.x <= blocks[i].x + BLOCK_WIDTH and !blocks[i].broken))
+			{
+				ball.up = false; // Bounce off bottom of block
+				break_block(blocks, i, score, remaining_blocks); // Break block
+			}
+            if (ball.y + BALL_RADIUS == blocks[i].y and (ball.x >= blocks[i].x and ball.x <= blocks[i].x + BLOCK_WIDTH and !blocks[i].broken)) 
+			{
+				ball.up = true; // Bounce off top of block
+				break_block(blocks, i, score, remaining_blocks); // Break block
+			}
+            if (ball.x - BALL_RADIUS == blocks[i].x + BLOCK_WIDTH and (ball.y >= blocks[i].y and ball.y <= blocks[i].y + BLOCK_HEIGHT and !blocks[i].broken)) 
+			{
+				ball.right = true; // Bounce off right of block
+				break_block(blocks, i, score, remaining_blocks); // Break block
+			}
+            if (ball.x + BALL_RADIUS == blocks[i].x and (ball.y >= blocks[i].y and ball.y <= blocks[i].y + BLOCK_HEIGHT and !blocks[i].broken))
+			{
+				ball.right = false; // Bounce off left of block
+				break_block(blocks, i, score, remaining_blocks); // Break block
+			}
         }
 
         // Update ball locations
@@ -108,12 +177,26 @@ int main()
         else ball.y += 1 * BALL_SPEED;
         if (ball.right) ball.x += 1 * BALL_SPEED;
         else ball.x -= 1 * BALL_SPEED;
+		
+		//win level if level is completed
+		if (remaining_blocks == 0)
+		{
+			game_won = true;
+			game_over = true;
+		}
         
         // Redraw everything
-        clear_screen(COLOR_BLACK);
-        draw_blocks(blocks, remaining_blocks); // Draw blocks
-        fill_circle(COLOR_WHITE, ball.x, ball.y, BALL_RADIUS); // Draw ball
-        fill_rectangle(COLOR_WHITE, paddle_x, PADDLE_Y, PADDLE_LENGTH, PADDLE_HEIGHT); // Draw paddle
+		clear_screen(COLOR_BLACK);
+		draw_blocks(blocks, remaining_blocks); // Draw blocks
+		if (!game_over) fill_circle(COLOR_WHITE, ball.x, ball.y, BALL_RADIUS); // Draw ball
+		fill_rectangle(COLOR_WHITE, paddle_x, PADDLE_Y, PADDLE_LENGTH, PADDLE_HEIGHT); // Draw paddle
+		draw_text("SCORE: " + to_string(score), COLOR_WHITE, font_named("default"), 20, 20, 20); // Draw score
+		
+		//draw win/lose messages when level ends
+		if (game_over)
+		{
+			end_level(game_won);
+		}
 
         refresh_screen(60);
     }
