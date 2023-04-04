44929daca6c13f330ba31f998d645f51cd8941ea
U-SWANOS\Swanos <swanny.aurellia21@gmail.com>
2023-04-04 18:02:25 +1000

    Level 1-5 and block types

--- games/DXBallGame/program.cpp
+++ games/DXBallGame/program.cpp
@@ -1,28 +1,52 @@
 #include "splashkit.h"
+#include <string>
+#include <iostream>
 using namespace std;
 /*
  * Todo:
  *
- * fix diagonal collisions (if the ball collides with a block diagonally it seems to pass through the block until it hits the center)
  * add more levels and ability to start again after losing
  * maybe add a game data struct to store game state info
+ * add powerups
  */
 
-const int BLOCKS_IN_LEVEL1 = 110; // Number of blocks to be spawned in level 1
+const int BLOCKS_IN_LEVEL1 = 32; // Number of blocks to be spawned in level 1
+const int BLOCKS_IN_LEVEL2 = 35;
+const int BLOCKS_IN_LEVEL3 = 44;
+const int BLOCKS_IN_LEVEL4 = 110;
+const int BLOCKS_IN_LEVEL5 = 120;
+
 const double BLOCK_WIDTH = 60; // Block width
 const double BLOCK_HEIGHT = 20; // Block height
-const double BALL_RADIUS = 10; // Radius of ball
-const double BALL_SPEED = 2; // Speed of ball movement
-const double PADDLE_SPEED = 5; //Speed of paddle movement
+const double BALL_RADIUS = 8; // Radius of ball
+const double BALL_SPEED = 4; // Speed of ball movement
+const double PADDLE_SPEED = 8; //Speed of paddle movement
 const double PADDLE_Y = 550; // Location of paddle on the y axis
 const double PADDLE_HEIGHT = 5; // Height of the paddle
 const double PADDLE_LENGTH = 100; // Length of the paddle
+
+
+enum block_kind
+{
+    SINGLE_HIT, // Bricks take one hit to be destroyed
+    DOUBLE_HIT, // Bricks take two hits to be destroyed
+    HIDDEN,     // Bricks hidden from the player before being hit, then take one more hit to be destroyed
+};
+
+enum powerups
+{
+    NO_POWERUP,     // No powerup
+};
 
 struct block_data
 {
     double x; // Location on the x axis
     double y; // Location on the y axis
-	bool broken = false; //whether or not the ball has hit the brick
+	//bool broken = false; //whether or not the ball has hit the brick
+    block_kind kind; // Type of brick
+    powerups powerup; // Powerup that appear after brick is destroyed
+    int hitpoint; // How many hits left to be destroyed
+    color color; // Color of block
 };
 
 struct ball_data
@@ -47,14 +71,38 @@ void end_level(bool successful)
 	}
 }
 
-void draw_blocks(block_data blocks[])
+block_data create_block(double x, double y, block_kind kind, powerups powerup)
+{
+    block_data block;
+    block.x = x;
+    block.y = y;
+    switch (kind)
+    {
+        case SINGLE_HIT:
+            block.hitpoint = 1;
+            block.color = COLOR_BLUE;
+            break;
+        case DOUBLE_HIT:
+            block.hitpoint = 2;
+            block.color = COLOR_YELLOW;
+            break;
+        case HIDDEN:
+            block.hitpoint = 2;
+            block.color = COLOR_BLACK;
+            break;
+    }
+    block.powerup = powerup;
+    return block;
+}
+
+void draw_blocks(block_data blocks[], int blocks_in_level)
 {
     // Draw blocks to the screen
-    for (int i = 0; i < BLOCKS_IN_LEVEL1; i++)
+    for (int i = 0; i < blocks_in_level; i++)
     {
-		if (!blocks[i].broken) //broken blocks aren't drawn
+		if (blocks[i].hitpoint > 0) //broken blocks aren't drawn
 		{
-			fill_rectangle(COLOR_BLUE, blocks[i].x, blocks[i].y, BLOCK_WIDTH, BLOCK_HEIGHT);
+			fill_rectangle(blocks[i].color, blocks[i].x, blocks[i].y, BLOCK_WIDTH, BLOCK_HEIGHT);
 			draw_rectangle(COLOR_BLACK, blocks[i].x, blocks[i].y, BLOCK_WIDTH, BLOCK_HEIGHT);
 		}
     }
@@ -62,9 +110,177 @@ void draw_blocks(block_data blocks[])
 
 void break_block(block_data blocks[], int block_index, int &score, int &remaining_blocks)
 {
-	blocks[block_index].broken = true;
+	blocks[block_index].hitpoint--;
 	score++;
-	remaining_blocks--;
+    if(blocks[block_index].hitpoint == 0)
+    {
+        remaining_blocks--;
+    }
+    else 
+    {
+        blocks[block_index].color = COLOR_BEIGE; // Change color of bricks hit but not destroyed yet
+    }
+
+    /*
+        Changing color by checking 
+        if (blocks[block_index].kind == DOUBLE_HIT)
+        always returns false for some reason
+    */
+}
+
+block_data * spawn_blocks_level1(int &remaining_blocks)
+{
+    remaining_blocks = BLOCKS_IN_LEVEL1; // Number of blocks remaining
+    static block_data blocks[BLOCKS_IN_LEVEL1]; // Array containing blocks to be destroyed
+    block_data block;
+    int index = 0;
+
+    for (int i = 0; i < 8; i++)
+    {
+        block = create_block(160 + i * BLOCK_WIDTH, 300, SINGLE_HIT, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        block = create_block(220 + i * BLOCK_WIDTH, 320, SINGLE_HIT, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+        block = create_block(220 + i * BLOCK_WIDTH, 280, SINGLE_HIT, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        block = create_block(280 + i * BLOCK_WIDTH, 340, SINGLE_HIT, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+        block = create_block(280 + i * BLOCK_WIDTH, 260, SINGLE_HIT, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        block = create_block(340 + i * BLOCK_WIDTH, 360, SINGLE_HIT, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+        block = create_block(340 + i * BLOCK_WIDTH, 240, SINGLE_HIT, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+    }
+
+    return blocks;
+}
+block_data * spawn_blocks_level2(int &remaining_blocks)
+{
+    static block_data blocks[BLOCKS_IN_LEVEL2];
+    remaining_blocks = BLOCKS_IN_LEVEL2;
+    int index = 0;
+    block_data block;
+
+    for (int i = 0; i < 7; i++)
+    {
+        block = create_block(40 + i * BLOCK_WIDTH, 300 - i * BLOCK_HEIGHT, SINGLE_HIT, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+        block = create_block(100 + i * BLOCK_WIDTH, 300 - i  * BLOCK_HEIGHT, DOUBLE_HIT, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+        block = create_block(280 + i * BLOCK_WIDTH, 300 - i * BLOCK_HEIGHT, SINGLE_HIT, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+        block = create_block(340 + i * BLOCK_WIDTH, 300 - i  * BLOCK_HEIGHT, DOUBLE_HIT, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        block = create_block(520 + i * BLOCK_WIDTH, 300 - i * BLOCK_HEIGHT, SINGLE_HIT, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+        block = create_block(580 + i * BLOCK_WIDTH, 300 - i  * BLOCK_HEIGHT, DOUBLE_HIT, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+    }
+    block = create_block(700, 240, SINGLE_HIT, NO_POWERUP);
+    blocks[index] = block;
+
+    return blocks;
+}
+block_data * spawn_blocks_level3(int &remaining_blocks)
+{
+    static block_data blocks[BLOCKS_IN_LEVEL3];
+    remaining_blocks = BLOCKS_IN_LEVEL3;
+    int index = 0;
+    block_data block;
+
+    for(int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            block = create_block(160 + j * BLOCK_WIDTH, 300 - i * BLOCK_HEIGHT, SINGLE_HIT, NO_POWERUP);
+            blocks[index] = block;
+            index++;
+        }
+    }
+    for (int i = 0; i < 12; i++)
+    {
+        block = create_block(40 + i * BLOCK_WIDTH, 360, HIDDEN, NO_POWERUP);
+        blocks[index] = block;
+        index++;
+    }
+
+    return blocks;
+}
+block_data * spawn_blocks_level4(int &remaining_blocks)
+{
+    remaining_blocks = BLOCKS_IN_LEVEL4; // Number of blocks remaining
+    static block_data blocks[BLOCKS_IN_LEVEL4]; // Array containing blocks to be destroyed
+    int index = 0;
+    block_data block;
+
+    for (int i = 0; i < 5; i++) // draw 10 layers
+    {
+        for (int j = 0; j < 10; j++) // draw 10 blocks
+        {
+            double x = 100 + j * BLOCK_WIDTH;
+            double y = 300 - i * 2 * BLOCK_HEIGHT;
+            block_kind kind = DOUBLE_HIT;
+            powerups powerup = NO_POWERUP;
+            block = create_block(x, y, kind, powerup);
+            blocks[index] = block;
+            index++;
+        }
+        for (int j = 0; j < 12; j++) // draw 12 blocks
+        {
+            double x = 40 + j * BLOCK_WIDTH;
+            double y = 300 - BLOCK_HEIGHT - i * 2 * BLOCK_HEIGHT;
+            block_kind kind = SINGLE_HIT;
+            powerups powerup = NO_POWERUP;
+            block = create_block(x, y, kind, powerup);
+            blocks[index] = block;
+            index++;
+        }
+    }
+    return blocks;
+}
+block_data * spawn_blocks_level5(int &remaining_blocks)
+{
+    static block_data blocks[BLOCKS_IN_LEVEL5];
+    remaining_blocks = BLOCKS_IN_LEVEL5;
+    block_data block;
+    int index = 0;
+    for(int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            block = create_block(40 + j * BLOCK_WIDTH, 300 - i * BLOCK_HEIGHT, static_cast<block_kind>(rand() % 3), NO_POWERUP);
+            blocks[index] = block;
+            index++;
+        }
+    }
+
+    return blocks;
 }
 
 int main()
@@ -73,33 +289,18 @@ int main()
     open_window("DX Ball Game", 800, 600);
     window_toggle_border("DX Ball Game");
 	
-    // Spawn blocks
-    int remaining_blocks = BLOCKS_IN_LEVEL1; // Number of blocks remaining
-	int score = 0;
-	bool game_over = false;
-	bool game_won = false;
-    block_data blocks[BLOCKS_IN_LEVEL1]; // Array containing blocks to be destroyed
-    int index = 0;
+    block_data *blocks;
+    int remaining_blocks;
+    int blocks_in_level;
+    int score = 0;
+    bool game_over = false;
+    bool game_won = false;
+    int current_level = 1;
+    bool next_level = false;
 
-    for (int i = 0; i < 5; i++) // draw 10 layers
-    {
-        for (int j = 0; j < 10; j++) // draw 10 blocks
-        {
-            block_data block; 
-            block.x = 100 + j * BLOCK_WIDTH;
-            block.y = 300 - i * 2 * BLOCK_HEIGHT;
-            blocks[index] = block;
-            index++;
-        }
-        for (int j = 0; j < 12; j++) // draw 12 blocks
-        {
-            block_data block;
-            block.x = 40 + j * BLOCK_WIDTH;
-            block.y = 300 - BLOCK_HEIGHT - i * 2 * BLOCK_HEIGHT;
-            blocks[index] = block;
-            index++;
-        }
-    }
+    // Spawn blocks
+    blocks = spawn_blocks_level1(remaining_blocks);
+    blocks_in_level = BLOCKS_IN_LEVEL1;
 	
 	// spawn only one block for testing winning a level
 	/*
@@ -110,23 +311,62 @@ int main()
 	*/
 	
     // Spawn ball at starting location
-    ball_data ball;
-    ball.x = screen_width()/2;
-    ball.y = 500;
-    ball.up = true;
-    ball.right = true;
+            ball_data ball;
+            ball.x = screen_width()/2;
+            ball.y = 500;
+            ball.up = true;
+            ball.right = true;
 
     // Paddle starting location at the x axis
     double paddle_x = (screen_width() - PADDLE_LENGTH) / 2;
 
     // Draw the environment
     clear_screen(COLOR_BLACK);
-    draw_blocks(blocks); // Draw blocks
+    draw_blocks(blocks, blocks_in_level); // Draw blocks
     fill_circle(COLOR_WHITE, ball.x, ball.y, BALL_RADIUS); // Draw ball
     fill_rectangle(COLOR_WHITE, paddle_x, PADDLE_Y, PADDLE_LENGTH, PADDLE_HEIGHT); // Draw paddle
 
     while(!key_down(ESCAPE_KEY))
     {
+        // Start level
+        if (next_level)
+        {
+            switch (current_level)
+            {
+                case 2: 
+                    blocks = spawn_blocks_level2(remaining_blocks);
+                    blocks_in_level = BLOCKS_IN_LEVEL2;
+                    break;
+                case 3: 
+                    blocks = spawn_blocks_level3(remaining_blocks);
+                    blocks_in_level = BLOCKS_IN_LEVEL3;
+                    break;
+                case 4:
+                    blocks = spawn_blocks_level4(remaining_blocks);
+                    blocks_in_level = BLOCKS_IN_LEVEL4;
+                    break;
+                case 5:
+                    blocks = spawn_blocks_level5(remaining_blocks);
+                    blocks_in_level = BLOCKS_IN_LEVEL5;
+                    break;
+                default:
+                    game_won = true;
+                    game_over = true;
+                    break;
+            }
+            
+            // Spawn ball at starting location
+            ball.x = screen_width()/2;
+            ball.y = 500;
+            ball.up = true;
+            ball.right = true;
+
+            // Paddle starting location at the x axis
+            paddle_x = (screen_width() - PADDLE_LENGTH) / 2;
+
+            next_level = false;
+        }
+
 		process_events(); //check keyboard state
 		
         // Player controls
@@ -145,22 +385,22 @@ int main()
 			game_over = true;
 		}
         // Bounce off paddle
-        if (ball.y + BALL_RADIUS == PADDLE_Y and (ball.x + BALL_RADIUS >= paddle_x and ball.x - BALL_RADIUS <= paddle_x + PADDLE_LENGTH))
+        if ((PADDLE_Y <= ball.y + BALL_RADIUS and ball.y + BALL_RADIUS <= PADDLE_Y + BALL_SPEED) and (ball.x + BALL_RADIUS >= paddle_x and ball.x - BALL_RADIUS <= paddle_x + PADDLE_LENGTH))
             ball.up = true;
         // Bounce off blocks
-        for (int i = 0; i < BLOCKS_IN_LEVEL1; i++)
+        for (int i = 0; i < blocks_in_level; i++)
         {
-            if (!blocks[i].broken)
+            if (blocks[i].hitpoint > 0)
             {
                 // Vertically
                 if (ball.x >= blocks[i].x and ball.x <= blocks[i].x + BLOCK_WIDTH)
                 {
-                    if (ball.y - BALL_RADIUS == blocks[i].y + BLOCK_HEIGHT) // Bottom of block
+                    if (blocks[i].y + BLOCK_HEIGHT - BALL_SPEED <= ball.y - BALL_RADIUS and ball.y - BALL_RADIUS <= blocks[i].y + BLOCK_HEIGHT) // Bottom of block
                     {
                         ball.up = false;
                         break_block(blocks, i, score, remaining_blocks);
                     }
-                    if (ball.y + BALL_RADIUS == blocks[i].y) // Top of block
+                    if (blocks[i].y <= ball.y + BALL_RADIUS and ball.y + BALL_RADIUS <= blocks[i].y + BALL_SPEED) // Top of block
                     {
                         ball.up = true;
                         break_block(blocks, i, score, remaining_blocks);
@@ -169,12 +409,12 @@ int main()
                 // Horizontally
                 if (ball.y + BALL_RADIUS >= blocks[i].y and ball.y - BALL_RADIUS < blocks[i].y + BLOCK_HEIGHT)
                 {
-                    if (ball.x - BALL_RADIUS == blocks[i].x + BLOCK_WIDTH) // Right of block
+                    if (blocks[i].x + BLOCK_WIDTH <= ball.x - BALL_RADIUS - BALL_SPEED and ball.x - BALL_RADIUS <= blocks[i].x + BLOCK_WIDTH) // Right of block
                     {
                         ball.right = true;
                         break_block(blocks, i, score, remaining_blocks);
                     }
-                    if (ball.x + BALL_RADIUS == blocks[i].x) // Left of block
+                    if (blocks[i].x <= ball.x + BALL_RADIUS and ball.x + BALL_RADIUS <= blocks[i].x + BALL_SPEED) // Left of block
                     {
                         ball.right = false;
                         break_block(blocks, i, score, remaining_blocks);
@@ -192,13 +432,13 @@ int main()
 		//win level if level is completed
 		if (remaining_blocks == 0)
 		{
-			game_won = true;
-			game_over = true;
+			next_level = true;
+            current_level++;
 		}
         
         // Redraw everything
 		clear_screen(COLOR_BLACK);
-		draw_blocks(blocks); // Draw blocks
+		draw_blocks(blocks, blocks_in_level); // Draw blocks
 		if (!game_over) fill_circle(COLOR_WHITE, ball.x, ball.y, BALL_RADIUS); // Draw ball
 		fill_rectangle(COLOR_WHITE, paddle_x, PADDLE_Y, PADDLE_LENGTH, PADDLE_HEIGHT); // Draw paddle
 		draw_text("SCORE: " + to_string(score), COLOR_WHITE, font_named("default"), 20, 20, 20); // Draw score
@@ -208,6 +448,9 @@ int main()
 		{
 			end_level(game_won);
 		}
+
+        // Shortcut button to change level for development purpose
+        //if (key_typed(NUM_1_KEY)) { next_level = true; current_level++; }
 
         refresh_screen(60);
     }
