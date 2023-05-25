66b8ce76dbe382e94513fdd43014cf25f953ca81
DaGoblin <s222574652@deakin.edu.au>
2023-05-25 10:58:00 +1000

    Moved Basic Demo into it's own folder

--- games/2dRacer/BasicDemo/Player.cs
+++ games/2dRacer/BasicDemo/Player.cs
@@ -5,13 +5,28 @@ using SplashKitSDK;
 // This class will be created with functions to be used in the game, as of creating this class none of these functions have been implemented into program.cs 
 
 
-public class Player : Program
+public class Player
 {
+    private Sprite _greenCarSolo;
+    private Sprite _greenCar1, _greenCar2;
+    private Window _GameWindow;
 
+    public Player(Window gameWindow, int playersNo)
+    {
+        _GameWindow = gameWindow;
+        if (playersNo == 1)
+        {
+            SpawnSolo();
+        }
+        else
+        {
+            SpawnDuo();
+        }
+
+    }
     public void SpawnSolo() //spawns a single car in the middle of the screen
     {
 
-        private sprite _greenCarSolo;
 
         Bitmap carBitmap = SplashKit.LoadBitmap("greenCar", "greenCar.png");
         carBitmap.SetCellDetails(75, 120, 3, 1, 3);
@@ -27,7 +42,7 @@ public class Player : Program
     public void SpawnDuo() // spawns 2 cars, each a lane away from the middle of the screen
     {
 
-        private sprite _greenCar1, _greenCar2
+
 
         Bitmap carBitmap = SplashKit.LoadBitmap("greenCar", "greenCar.png");
         carBitmap.SetCellDetails(75, 120, 3, 1, 3);
@@ -50,89 +65,89 @@ public class Player : Program
         {
 
             int Speed = 2; //speed can be changed to configure the games difficulty during testing
-            
-            if (SplashKit.KeyDown(KeyCode.LeftKey) & _greenCarSolo.AnimationHasEnded) 
+
+            if (SplashKit.KeyDown(KeyCode.LeftKey) & _greenCarSolo.AnimationHasEnded)
             {
                 _greenCarSolo.StartAnimation("left");
-                _greenCarSolo.Dx = -Speed;
+                // _greenCarSolo.Dx = -Speed;
             }
-            if (SplashKit.KeyDown(KeyCode.RightKey) & _greenCarSolo.AnimationHasEnded) 
+            if (SplashKit.KeyDown(KeyCode.RightKey) & _greenCarSolo.AnimationHasEnded)
             {
                 _greenCarSolo.StartAnimation("right");
-                _greenCarSolo.Dx = Speed;
+                // _greenCarSolo.Dx = Speed;
             }
-            if (SplashKit.KeyReleased(KeyCode.RightKey) || SplashKit.KeyReleased(KeyCode.LeftKey)) 
+            if (SplashKit.KeyReleased(KeyCode.RightKey) || SplashKit.KeyReleased(KeyCode.LeftKey))
             {
                 _greenCarSolo.StartAnimation("straight");
             }
-            if (SplashKit.KeyDown(KeyCode.UpKey) & _greenCarSolo.AnimationHasEnded) 
+            if (SplashKit.KeyDown(KeyCode.UpKey) & _greenCarSolo.AnimationHasEnded)
             {
-                _greenCarSolo.Dy = -Speed;
+                // _greenCarSolo.Dy = -Speed;
             }
             if (SplashKit.KeyReleased(KeyCode.UpKey))
             {
-                _greenCarSolo.Dy = Speed;
+                // _greenCarSolo.Dy = Speed;
             }
 
         }
 
         if (IsSolo == false)
         {
-            
+
             int Speed = 2; //speed can be changed to configure the games difficulty during testing
-            
-            if (SplashKit.KeyDown(KeyCode.AKey) & _greenCar1.AnimationHasEnded) 
+
+            if (SplashKit.KeyDown(KeyCode.AKey) & _greenCar1.AnimationHasEnded)
             {
                 _greenCar1.StartAnimation("left");
-                _greenCar1.Dx = -Speed;
+                //_greenCar1.Dx = -Speed;
             }
-            if (SplashKit.KeyDown(KeyCode.DKey) & _greenCar1.AnimationHasEnded) 
+            if (SplashKit.KeyDown(KeyCode.DKey) & _greenCar1.AnimationHasEnded)
             {
                 _greenCar1.StartAnimation("right");
-                _greenCar1.Dx = Speed;
+                //_greenCar1.Dx = Speed;
             }
-            if (SplashKit.KeyReleased(KeyCode.DKey) || SplashKit.KeyReleased(KeyCode.AKey)) 
+            if (SplashKit.KeyReleased(KeyCode.DKey) || SplashKit.KeyReleased(KeyCode.AKey))
             {
                 _greenCar1.StartAnimation("straight");
             }
-            if (SplashKit.KeyDown(KeyCode.WKey) & _greenCar1.AnimationHasEnded) 
+            if (SplashKit.KeyDown(KeyCode.WKey) & _greenCar1.AnimationHasEnded)
             {
-                _greenCar1.Dy = -Speed;
+                //_greenCar1.Dy = -Speed;
             }
             if (SplashKit.KeyReleased(KeyCode.WKey) & _greenCar1.AnimationHasEnded)
             {
-                _greenCar1.Dy = Speed;
+                //_greenCar1.Dy = Speed;
             }
-            
-            
-            
-            if (SplashKit.KeyDown(KeyCode.LeftKey) & _greenCar2.AnimationHasEnded) 
+
+
+
+            if (SplashKit.KeyDown(KeyCode.LeftKey) & _greenCar2.AnimationHasEnded)
             {
                 _greenCar2.StartAnimation("left");
-                _greenCar2.Dx = -Speed;
+                //_greenCar2.Dx = -Speed;
             }
-            if (SplashKit.KeyDown(KeyCode.RightKey) & _greenCar2.AnimationHasEnded) 
+            if (SplashKit.KeyDown(KeyCode.RightKey) & _greenCar2.AnimationHasEnded)
             {
                 _greenCar2.StartAnimation("right");
-                _greenCar2.Dx = Speed;
+                //_greenCar2.Dx = Speed;
             }
-            if (SplashKit.KeyReleased(KeyCode.RightKey) || SplashKit.KeyReleased(KeyCode.LeftKey)) 
+            if (SplashKit.KeyReleased(KeyCode.RightKey) || SplashKit.KeyReleased(KeyCode.LeftKey))
             {
                 _greenCar2.StartAnimation("straight");
             }
-            if (SplashKit.KeyDown(KeyCode.UpKey) & _greenCar2.AnimationHasEnded) 
+            if (SplashKit.KeyDown(KeyCode.UpKey) & _greenCar2.AnimationHasEnded)
             {
-                _greenCar2.Dy = -Speed;
+                _greenCar2.Dy = 0;
             }
             if (SplashKit.KeyReleased(KeyCode.UpKey) & _greenCar2.AnimationHasEnded)
             {
-                _greenCar2.Dy = Speed;
+                _greenCar2.Dy = 0;
             }
 
 
         }
-        
-        
+
+
 
     }
 
