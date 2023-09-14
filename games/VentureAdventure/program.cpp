65156e2392c77f46a60f0a4046e8a5100d0b3dd8
RileyOussoren <127369591+RileyOussoren@users.noreply.github.com>
2023-09-14 16:28:56 +1000

    Update program.cpp

--- games/VentureAdventure/program.cpp
+++ games/VentureAdventure/program.cpp
@@ -17,8 +17,13 @@ int main()
     window_toggle_border("VentureAdventure");
     load_resources();
 
+    string map = "level1.txt";
+    int level = 1;
+    bool win;
+    string levelnum = "Level 1";
+    
     game_data game;
-    game = new_game();
+    game = new_game(map);
 
     while(!quit_requested() && !key_down(ESCAPE_KEY)) 
     {
@@ -40,7 +45,7 @@ int main()
 
             draw_game(game);
 
-            update_game(game);
+            win = update_game(game, levelnum);
 
             if(game.player.attacked == true)
                 draw_text("Game Over", COLOR_BLANCHED_ALMOND, "font.ttf", 70, SCREEN_WIDTH/2-138, SCREEN_HEIGHT/2-48, option_to_screen());
@@ -48,6 +53,40 @@ int main()
             start_debug(game);
 
             refresh_screen(60);
+
+            if (win == true && level < 3 )
+            {
+                level++;
+                map = "level";
+                map.append(to_string(level));
+                map.append(".txt");
+                levelnum = "Level ";
+                levelnum.append(to_string(level));
+                game = new_game(map);
+            };
+
+            if (win == true && level == 3)
+            {
+                delay(5000);
+                
+                play_music("intro");
+                set_music_volume(0.04);
+
+                start_screen();
+    
+                fade_music_out(1000);
+
+                play_music("game", 100);
+                set_music_volume(0.025);
+
+                level = 1;
+                map = "level";
+                map.append(to_string(level));
+                map.append(".txt");
+                levelnum = "Level ";
+                levelnum.append(to_string(level));
+                game = new_game(map);
+            };
         }
    }
 
