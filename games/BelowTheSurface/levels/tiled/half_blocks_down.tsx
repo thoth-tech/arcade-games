518a390f07da7dfbc66f616c44fe565ff5bef399
Bullet-Not-Proof <57785358+Bullet-Not-Proof@users.noreply.github.com>
2024-05-06 16:14:32 +1000

    level editor below the surface

---
+++ games/BelowTheSurface/levels/tiled/half_blocks_down.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="half_blocks_down" class="Types" tilewidth="64" tileheight="32" tilecount="6" columns="2" fillmode="preserve-aspect-fit">
+ <properties>
+  <property name="Offset" type="int" value="201"/>
+ </properties>
+ <image source="../../Resources/images/half_blocks.png" width="128" height="96"/>
+</tileset>
---
+++ games/BelowTheSurface/levels/tiled/half_blocks_up.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="half_blocks_up" class="Types" tilewidth="64" tileheight="32" tilecount="6" columns="2" fillmode="preserve-aspect-fit">
+ <tileoffset x="0" y="-32"/>
+ <properties>
+  <property name="Offset" type="int" value="101"/>
+ </properties>
+ <image source="../../Resources/images/half_blocks.png" width="128" height="96"/>
+</tileset>
