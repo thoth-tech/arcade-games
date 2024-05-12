e7cdc20cb4f18eacb419c92d3b9e253d7cabd31f
CastielM <78852960+CastielM@users.noreply.github.com>
2024-05-12 23:26:41 +1000

    CastielM added new level. Level 0, causing a fault

--- games/VentureAdventure/program.cpp
+++ games/VentureAdventure/program.cpp
@@ -18,9 +18,9 @@ int main()
     load_resources();
 
     //use this to adjust starting level (for debugging) or to change total number of levels when adding new levels
-    string map = "Resources/levels/level1.txt";
+    string map = "Resources/levels/level0.txt";
     int totallevels = 3;
-    int level = 1;
+    int level = 0;
     bool win;
     int startinglives = 3;
     int currentlives = startinglives;
