770d35acc94e39177adfd77369d9dcf0e0a51c69
KhushiLaddi08 <laddikhushi43@gmail.com>
2024-05-13 23:43:37 +1000

    Updated code for car-race

--- games/car-race/game.h
+++ games/car-race/game.h
@@ -4,7 +4,8 @@
 #include "splashkit.h"
 #include <vector>
 #include "car.h"
-
+#include "powerup.h"
+#include "barricade.h"
 // Structure to store game data
 struct game_data
 {
@@ -13,6 +14,16 @@ struct game_data
     car_data car;             // Player's car
     vector<car_data> cars;    // Vector of obstacle cars
     double score;             // Current score
+    vector<powerup_data> powerups; // Vector of powerups
+    int level;                // Current level
+    bool shield;              // Flag indicating if the player has a shield
+    bool speed_boost;         // Flag indicating if the player has a speed boost
+    vector<barricade_data> barricades; // Vector of barricades
+    car_data secondPlayer;
+    bool twoPlayer;
+    bool paused;
+    bool finished;
+    sprite finish_line;
 };
 
 // Function to create a new game
@@ -30,4 +41,8 @@ void out_range(game_data &game);
 // Function to check for collisions between the player's car and obstacle cars
 void check_collisions(game_data &game);
 
+// check_collisions_barricade(game);
+void check_collisions_barricade(game_data &game);
+//    check_collisions_powerup(game);
+void check_collisions_powerup(game_data &game);
 #endif
